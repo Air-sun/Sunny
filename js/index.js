076552c424ac5694574eb79b3ceac48a75e58513
Sunny <984975974@qq.com>
2021-10-31 16:29:00 +0800

    上传图片功能完成

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ $(function () {
         $('.p1-w2 .tab-txt li').eq($(this).index()).addClass('active').siblings().removeClass('active');
         $('.p1-w3 .tab-txt li').eq($(this).index()).addClass('active').siblings().removeClass('active');
     })
-    // 点击上传图片
+    // 点击选择图片
     $('.p1-w2 .w2-camera #files').change(function () {
         var fd = new FormData();
         fd.append('avatar', $(this)[0].files[0]);
@@ -48,17 +48,29 @@ $(function () {
             }
         })
     })
-    // 点击进入保存图片页
+    // 点击上传图片
     $('.w2-sure').click(function () {
         if ($('.p1-w2 .w2-camera #files')[0].files.length <= 0) {
             return alert('请选择文件!');
         }
+        $('.p1-w2 .tab-item .w2-camera').hide();
+        //合成图片的js代码
+        html2canvas(document.querySelector('.tab-item'), {
+            useCORS: true,
+            //使用看到的宽高作为图片的宽高
+            width: 501,
+            height: 791
+        }).then(canvas => {
+            //合成图片放到img中
+            let src = canvas.toDataURL();
+            $('.p1-w3 .tab-item img').attr('src', src);
+        });
         $('.p1-w2').fadeOut().next().fadeIn();
     })
     // 点击进入排行榜
     $('.w3-rank').click(function () {
         $('.p1-w3').fadeOut().next().fadeIn();
-        $('.w4-layer').fadeIn(3000);
+        $('.w4-layer').fadeIn(1500);
     })
     // 点击关闭弹层
     $('.w4-layer .close').click(function () {
@@ -69,9 +81,20 @@ $(function () {
         $('.p1-w3').fadeIn().next().fadeOut();
     })
     // 图片拖拽
-    $('.tab-img').on('touchstart', function () {
-        $('.tab-img img').on('touchmove', function () {
-            console.log($(this).position());
+    $('.tab-img').on('touchstart', function (e) {
+        var X = e.targetTouches[0].pageX;
+        var Y = e.targetTouches[0].pageY;
+        var startLeft = $(this).children().offset().left;
+        var startTop = $(this).children().offset().top;
+        $('.tab-img img').on('touchmove', function (e) {
+            var moveX = e.targetTouches[0].pageX - X;
+            var moveY = e.targetTouches[0].pageY - Y;
+            $(this).offset({
+                left: startLeft + moveX,
+                top: startTop + moveY
+            })
         })
     })
+
+
 })
